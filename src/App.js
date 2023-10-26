03aa4b3125e3a4eb21c88dd4f20b54e2c24c6e67
sanin-mn <msanin7003@gmail.com>
2023-10-26 22:59:06 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  
+const [color,setColor] = useState("white")
+
+  const redColor = ()=>{
+    setColor('red')
+  }
+  const blueColor = ()=>{
+    setColor('blue')
+  }
+  const greenColor = ()=>{
+    setColor('green')
+  }
+  const yellowColor = ()=>{
+    setColor('yellow')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='main-bg d-flex align-items-center justify-content-center' style={{backgroundColor:color}}>
+   
+
+      <div className='mt-5'>
+
+        <button className='btn btn-danger m-2 ' onClick={redColor}>Red</button>
+        <button className='btn btn-info m-2 ' onClick={blueColor}>Blue</button>
+        <button className='btn btn-success m-2 ' onClick={greenColor}>Green</button>
+        <button className='btn btn-warning m-2 ' onClick={yellowColor}>Yellow</button>
+
+      </div>
+
+
     </div>
   );
 }
